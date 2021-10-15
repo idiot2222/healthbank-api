f222bf0b8e8a07bda8de91ab153805cdcce1894f
Bogeun <pky2892@gmail.com>
2021-10-16 05:34:02 +0900

    app 모듈에 bootstrap 추가

---
+++ app/src/main/java/com/pps/HealthBankApplication.java
@@ -0,0 +1,11 @@
+package com.pps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthBankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HealthBankApplication.class);
+    }
+}
