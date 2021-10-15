5369b60848cd01a527bccb67d2865100b74901ef
Bogeun <pky2892@gmail.com>
2021-10-16 05:19:42 +0900

    added RegistrationJpaAdapter

--- infra/src/main/java/com/pps/entity/Registration.java
+++ infra/src/main/java/com/pps/entity/Registration.java
@@ -1,10 +1,15 @@
 package com.pps.entity;
 
+import com.pps.data.RegistrationDto;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@NoArgsConstructor
 @Entity
 public class Registration {
 
@@ -15,4 +20,19 @@ public class Registration {
     private Long userId;
     private Long trainerId;
 
+
+
+    @Builder
+    public Registration(Long userId, Long trainerId) {
+        this.userId = userId;
+        this.trainerId = trainerId;
+    }
+
+    public RegistrationDto convertToRegistrationDto() {
+        RegistrationDto dto = new RegistrationDto();
+        dto.setUserId(this.userId);
+        dto.setTrainerId(this.trainerId);
+
+        return dto;
+    }
 }
