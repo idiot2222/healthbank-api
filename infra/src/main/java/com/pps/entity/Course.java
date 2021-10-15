b5428d498eef4e7d322d5aaf81d25106a8c63f3a
Bogeun <pky2892@gmail.com>
2021-10-16 05:03:31 +0900

    added CourseJpaAdapter

--- infra/src/main/java/com/pps/entity/Course.java
+++ infra/src/main/java/com/pps/entity/Course.java
@@ -1,10 +1,15 @@
 package com.pps.entity;
 
+import com.pps.data.CourseDto;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@NoArgsConstructor
 @Entity
 public class Course {
 
@@ -17,5 +22,26 @@ public class Course {
     private int limit;
 
     private Long trainerId;
+
+
+
+    @Builder
+    public Course(String title, String description, int limit, Long trainerId) {
+        this.title  = title;
+        this.description = description;
+        this.limit = limit;
+        this.trainerId = trainerId;
+    }
+
+    public CourseDto convertToCourseDto() {
+        CourseDto dto = new CourseDto();
+        dto.setId(this.id);
+        dto.setTitle(this.title);
+        dto.setDescription(this.description);
+        dto.setLimit(this.limit);
+        dto.setTrainerId(this.trainerId);
+
+        return dto;
+    }
 
 }
--- infra/src/main/java/com/pps/repository/CourseRepository.java
+++ infra/src/main/java/com/pps/repository/CourseRepository.java
@@ -3,5 +3,10 @@ package com.pps.repository;
 import com.pps.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByTrainerId(Long trainerId);
+
 }
