3ae555173c98778e408908fc8fe46424f0fc6ca3
Bogeun <pky2892@gmail.com>
2021-10-16 03:34:46 +0900

    added incoming, outgoing ports

---
+++ domain/src/main/java/com/pps/ports/incoming/CourseServicePort.java
@@ -0,0 +1,23 @@
+package com.pps.ports.incoming;
+
+import com.pps.data.CourseDto;
+
+import java.util.List;
+
+public interface CourseServicePort {
+
+    CourseDto createCourse(CourseDto courseDto);
+
+    CourseDto getCourseById(Long courseId);
+
+    List<CourseDto> getAllCourseList(Long courseId);
+
+    List<CourseDto> getCourseListByUserId(Long userId);
+
+    List<CourseDto> getCourseListByTrainerId(Long trainerId);
+
+    CourseDto updateCourse(CourseDto courseDto);
+
+    void deleteCourse(Long courseId);
+
+}
