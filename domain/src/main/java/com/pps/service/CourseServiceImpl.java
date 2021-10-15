5a02f585369f48e7c6e8e450ccb8e0551ec70daa
Bogeun <pky2892@gmail.com>
2021-10-16 03:38:01 +0900

    added CourseServiceImpl

---
+++ domain/src/main/java/com/pps/service/CourseServiceImpl.java
@@ -0,0 +1,50 @@
+package com.pps.service;
+
+import com.pps.data.CourseDto;
+import com.pps.ports.incoming.CourseServicePort;
+import com.pps.ports.outgoing.CoursePersistencePort;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CourseServiceImpl implements CourseServicePort {
+
+    private final CoursePersistencePort persistencePort;
+
+    @Override
+    public CourseDto createCourse(CourseDto courseDto) {
+        return persistencePort.createCourse(courseDto);
+    }
+
+    @Override
+    public CourseDto getCourseById(Long courseId) {
+        return persistencePort.getCourseById(courseId);
+    }
+
+    @Override
+    public List<CourseDto> getAllCourseList(Long courseId) {
+        return persistencePort.getAllCourseList(courseId);
+    }
+
+    @Override
+    public List<CourseDto> getCourseListByUserId(Long userId) {
+        return persistencePort.getCourseListByUserId(userId);
+    }
+
+    @Override
+    public List<CourseDto> getCourseListByTrainerId(Long trainerId) {
+        return persistencePort.getCourseListByTrainerId(trainerId);
+    }
+
+    @Override
+    public CourseDto updateCourse(CourseDto courseDto) {
+        return persistencePort.updateCourse(courseDto);
+    }
+
+    @Override
+    public void deleteCourse(Long courseId) {
+        persistencePort.deleteCourse(courseId);
+    }
+
+}
